6363672f9b2233169ea0ec65c20e268dad3c8f71
saifulislam888911 <saiful.islam888911@gmail.com>
2025-07-25 23:13:22 +0600

    Commit No. 1 - Live-Test-1 (Complete) - commit 1

---
+++ Live-Test-1/OOP_in_Csharp/Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_in_Csharp
+{
+    internal class Book
+    {
+        // Encapsulation : Access Modifier used for codes visibility and accessing Properties & methods 
+        public string Title {  get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        public bool IsAvailable { get; set; }
+
+        // Constructor : Parameterized Constructor has been used here.Arguments value sent to the parameters and properties values have been assigned here.
+        public Book(string title, string author, int pages, bool isAvailable = true) 
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Pages = pages;
+            this.IsAvailable = isAvailable;
+        }
+
+        public void BorrowBook() 
+        {
+            IsAvailable = false;
+
+            Console.WriteLine("Book has been borrowed.");
+        }
+
+        public void ReturnBook()
+        {
+            IsAvailable = true;
+
+            Console.WriteLine("Book has been returned");
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Title: {Title}, Author: {Author}, Pages: {Pages}, Available: {IsAvailable}");
+        }
+    }
+}
---
+++ Live-Test-1/OOP_in_Csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_in_Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Object : Book class's new object has been created and arguments values have been passed to the constructor.
+            Book book = new Book("Deyal", "Humayun Ahmed", 195);
+            book.DisplayInfo();
+            book.BorrowBook();
+            book.DisplayInfo();
+            book.ReturnBook();
+            book.DisplayInfo();
+        }
+    }
+}
